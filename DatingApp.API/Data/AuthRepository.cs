558d39aa64addc1071dc6795c0c4373073e1baf7
MyX220 <y_nomoto@tkc.att.ne.jp>
2019-02-11 20:02:02 +0900

    Login Method Completed

--- DatingApp.API/Data/AuthRepository.cs
+++ DatingApp.API/Data/AuthRepository.cs
@@ -1,6 +1,9 @@
 using System;
 using System.Threading.Tasks;
 using DatingApp.API.Models;
+using DatingApp.API.Data;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace DatingApp.API.Data
 {
@@ -12,9 +15,29 @@ namespace DatingApp.API.Data
             this._context = context;
 
         }
-        public Task<User> Login(string username, string password)
+        public async Task<User> Login(string username, string password)
         {
-            throw new System.NotImplementedException();
+            var user = await _context.Users.FirstOrDefaultAsync(x => x.Username == username);
+
+            if(user == null)
+                return null;
+
+            if(!VerifyPasswordHash(password , user.PasswordHash , user.PasswordSalt))
+                return null;
+
+            return user;  
+
+        }
+
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using(var hamac = new System.Security.Cryptography.HMACSHA512(passwordSalt)){
+                var computedHash = hamac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                for(int i = 0 ; i < computedHash.Length ; i++){
+                    if(computedHash[i] != passwordHash[i]) return false;
+                }
+            }
+            return true;
         }
 
         public async Task<User> Register(User user, string password)
@@ -42,9 +65,12 @@ namespace DatingApp.API.Data
             
         }
 
-        public Task<bool> UserExists(string username)
+        public async Task<bool> UserExists(string username)
         {
-            throw new System.NotImplementedException();
+            if(await _context.Users.AnyAsync(x => x.Username == username))
+                return true;
+            
+            return false;
         }
     }
 }
---
+++ Readme.txt
@@ -0,0 +1,3 @@
+2019/02/12
+i have to install vscode on t460s and add extensions
+more , install node_modules from package .json
