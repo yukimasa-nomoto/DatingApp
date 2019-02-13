84d3fcfebced69d333518771cd5b4cc40ab751cc
野元之雅 <yukimasa_nomoto@csc1980.co.jp>
2019-02-13 16:43:08 +0900

    Section4 Complete

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -21,7 +21,16 @@ export class NavComponent implements OnInit {
     }, error => {
       console.log('Failed to login');
     });
-
   }
 
+  loggedIn() {
+    const token = localStorage.getItem('token');
+    return !!token;
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    console.log('log out');
+
+  }
 }
